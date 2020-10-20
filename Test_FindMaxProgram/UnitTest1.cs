22448e5d7d3c0d411f829b9c618ffa804f93ce93
JR Mishra <jrmnitdgp@gmail.com>
2020-10-20 21:05:56 +0530

    [JR Mishra]Test. UC4 test cases passed

--- Test_FindMaxProgram/UnitTest1.cs
+++ Test_FindMaxProgram/UnitTest1.cs
@@ -10,10 +10,13 @@ namespace Test_FindMaxProgram
         [DataRow(8,3,21)]
         [DataRow(3,8,21)]
         [DataRow(3,21,8)]
-        public void MaxIntMethod_ShouldReturnMaxInteger(int n1, int n2, int n3)
+        [DataRow(21)]
+        [DataRow(21,3,11,8,17)]
+        [DataRow(7,11,21,8)]
+        public void MaxIntMethod_ShouldReturnMaxInteger(params int[] values)
         {
             //arrange
-            Comparision<int> compareInt = new Comparision<int>(n1, n2, n3);
+            Comparision<int> compareInt = new Comparision<int>(values);
             int expectedMax = 21;
 
             //act
@@ -27,10 +30,13 @@ namespace Test_FindMaxProgram
         [DataRow(8.5f,13.2f,1.7f)]
         [DataRow(13.2f,8.5f,1.7f)]
         [DataRow(13.2f,1.7f,8.5f)]
-        public void MaxFloatMethod_ShouldReturnMaxFloat(float f1, float f2, float f3)
+        [DataRow(13.2f)]
+        [DataRow(13.2f, 3.6f, 11.4f, 8.1f, 7.9f)]
+        [DataRow(7.9f, 11.3f, 13.2f, 8.5f)]
+        public void MaxFloatMethod_ShouldReturnMaxFloat(params float[] values)
         {
             //arrange
-            Comparision<float> compareFloat = new Comparision<float>(f1, f2, f3);
+            Comparision<float> compareFloat = new Comparision<float>(values);
             float expectedMax = 13.2f;
 
             //act
@@ -44,10 +50,12 @@ namespace Test_FindMaxProgram
         [DataRow("Corona", "Covid", "SarsCov2")]
         [DataRow("SarsCov2", "Covid", "Corona" )]
         [DataRow("Covid", "SarsCov2", "Corona")]
-        public void MaxStringMethod_ShouldReturnMaxString(string s1, string s2, string s3)
+        [DataRow("SarsCov2", "Corona", "Covid", "pandemic","lockdown")]
+        [DataRow("Covid", "SarsCov2", "Corona","pandemic")]
+        public void MaxStringMethod_ShouldReturnMaxString(params string[] values)
         {
             //arrange
-            Comparision<string> compareString = new Comparision<string>(s1,s2,s3);
+            Comparision<string> compareString = new Comparision<string>(values);
             string expectedMax = "SarsCov2";
             
             //act
